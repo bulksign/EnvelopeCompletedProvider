8c5bdaec6ac3c5f856b71de4ad7ee91a3f7ae4c1
Bulksign <contact@bulksign.com>
2018-07-11 18:41:44 +0300

    added sample code

---
+++ BundleFinishedProvider/LogBundleFinished.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Bulksign.Extensibility;
+
+namespace BundleFinishedProvider
+{
+	public class LogBundleFinished : IBundleFinished
+	{
+		public Dictionary<string, string> Settings
+		{
+			get;
+			set;
+		}
+
+		public string ProviderName => "LogBundleFinishedProvider";
+
+		public event LogDelegate Log;
+
+		public void Process(string bundlePublicId, string archiveFilePath)
+		{
+			//just log the received information. Replace this and copy the file, back it up or read the file content and sent it to another service
+			Log(LogLevel.Info, null, $"Received bundle finished notification for bundle with id {bundlePublicId}, file path is {archiveFilePath}");
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BundleFinishedProvider
-Example for implementing a bundle finished provider for Bulksign platform
+Example for implementing a bundle finished provider for Bulksign platform. This provider just logs the received id and file path.
+
